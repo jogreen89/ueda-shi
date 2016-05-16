52675e1a01f812fd49052df2d7dc51913e457d7e
zubernetes <zubernetes@gmail.com>
2016-05-16 18:27:01 -0400

    Adding some classes to operate on numbers.

---
+++ BinarySearch/Main.java
@@ -0,0 +1,10 @@
+class Main {
+
+    public static void main(String args[]) {
+        Numbers n = new Numbers(10, 20);
+        System.out.println(n.getX());
+        System.out.println(n.getY());
+        
+        System.out.println("Hello, World");
+    }
+}
---
+++ BinarySearch/Numbers.java
@@ -0,0 +1,21 @@
+class Numbers {
+    private int x = 0;
+    private int y = 0;
+
+    Numbers() {
+        x = 0;
+        y = 0;
+    }
+    Numbers(int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
